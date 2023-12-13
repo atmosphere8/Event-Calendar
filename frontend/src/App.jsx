95cb4af4f9d57c331e29c374bd7598eb6bad5578
atmosphere8 <azurrodneyyy@gmail.com>
2023-12-13 12:10:49 +0200

    Second Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,156 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css';
+import Arrow from './images/Arrow.jpg';
+import Vector from './images/Vector.png';
+import Cort from './images/Cort.png';
+import JazzIsDead from './images/JazzIsDead.png';
+import SuperTight from './images/SuperTight.png';
+import Point from './images/Point.png';
+import Video from './videos/videoplayback.mp4';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  
+      <body>
+        <header>
+          <nav>
+            <div className='leftContainer'>
+              <a className='leftNav' href="">Logo</a> 
+              <a className='leftNav' href="">Events</a>
+              <a className='leftNav' href="">Propose Event</a>
+            </div>
+            <div className='rightContainer'>
+              <a className='rightNav' href="">New York</a> <img className='rightNavImgs' src={Arrow} alt="Arrow"/> 
+              <a className='rightNav' href="">24.06.2022</a> <img className='rightNavImgs' src={Arrow} alt="Arrow"/>  
+              <a className='rightNav' href="">Rock Concert</a> <img className='rightNavImgs' src={Arrow} alt="Arrow"/> 
+            </div>
+          </nav>
+        </header>
+
+        <div className='bodyContainer'>
+          <h1 className='firstText'>Now is the time to Pick a cool event and let&apos;s go party!</h1>
+
+          <div className='eventContainer'>
+
+              <h2 className='topEvents'>TOP 5 EVENTS</h2> 
+
+            <div className='eventVectorContainer'>
+              <img className='eventVectorLeftImage' src={Vector} alt="Vector"/>
+              <img className='eventVectorRightImage' src={Vector} alt="Vector"/>
+            </div>
+
+          
+          </div>
+            <div className='eventRectangles'>
+
+              <div className='rectangleOne'>
+                <img className='rectImage' src={SuperTight} alt="SuperTight"/>
+
+                <div className='headerTextRect'>
+                  <img className='pointImage' src={Point} alt="Point"/>
+                  <p className='textNextToPoint'>SUBSTANCE SKATEPARK BROOKLYN</p>
+                </div>
+
+                <div className='bodyTextRect'>
+                  <p className='textUnderPoint'>NPCC SUMMER FEST 2022: SUPER TIGHT</p>
+                </div>
+
+
+                <div className='infoTextContainer'>
+                  <p className='infoTextRect'>
+                    Lörem ipsum tiselogi kälig mår. Mono konat och antekadören desm. Prena nås.
+                    Ovuning ren tetralig mivis tidat samt resk. Dekare teledåde inteр antekadör!
+                  </p>
+                </div>
+
+                <div className='aboutEventContainer'>
+                     <a href="" className='aboutEventHyperlink'>
+                      <div className='buttonText'>
+                        More about event
+                      </div>
+                     </a>
+                </div>
+
+              </div>
+
+              <div className='rectangleTwo'>
+                <img className='rectImage' src={JazzIsDead} alt="JazzIsDead"/>
+
+                <div className='headerTextRect'>
+                  <img className='pointImage' src={Point} alt="Point"/>
+                  <p className='textNextToPoint'>MUSIC HALL OF WILLIAMSBURG | BROOKLYN</p>
+                </div>
+
+                <div className='bodyTextRect'>
+                  <p className='textUnderPoint'>JAZZ IS DEAD WITH EBO TAYLOR</p>
+                </div>
+
+                <div className='infoTextContainer'>
+                  <p className='infoTextRect'>
+                    Lörem ipsum tiselogi kälig mår. Mono konat och antekadören desm. Prena nås.
+                    Ovuning ren tetralig mivis tidat samt resk. Dekare teledåde inteр antekadör!
+                  </p>
+                </div>
+
+                <div className='aboutEventContainer'>
+                     <a href="" className='aboutEventHyperlink'>
+                      <div className='buttonText'>
+                        More about event
+                      </div>
+                     </a>
+                </div>        
+
+              </div>
+
+              <div className='rectangleThree'>
+                <img className='rectImage' src={Cort} alt="Cort"/>
+
+                <div className='headerTextRect'>
+                  <img className='pointImage' src={Point} alt="Point"/>
+                  <p className='textNextToPoint'>SUBSTANCE SKATEPARK BROOKLYN</p>
+                </div>
+
+                <div className='bodyTextRect'>
+                  <p className='textUnderPoint'>JAZZ IS DEAD WITH EBO TAYLOR</p>
+                </div>
+
+                <div className='infoTextContainer'>
+                  <p className='infoTextRect'>
+                    Lörem ipsum tiselogi kälig mår. Mono konat och antekadören desm. Prena nås.
+                    Ovuning ren tetralig mivis tidat samt resk. Dekare teledåde inteр antekadör!
+                  </p>
+                </div>
+
+                <div className='aboutEventContainer'>
+                     <a href="" className='aboutEventHyperlink'>
+                      <div className='buttonText'>
+                        More about event
+                      </div>
+                     </a>
+                </div>
+
+              </div>
+
+          </div>
+        </div>
+
+        <div className='videoContainer'>
+          <h2 className='videoText'>Videos</h2>
+            <div className='videoBox'>
+              <video className='videoPlay' controls="controls"src={Video}></video>
+            </div>
+        </div>
+    
+        <div className="lastLine">
+          <svg>
+            <line x1="0" y1="0" x2="100%" y2="0"></line>
+          </svg>
+        </div>
+
+        <footer>
+          
+        </footer>
+      </body>
     </>
   )
 }
