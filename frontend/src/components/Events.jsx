04c0332eac091a7f97dcce209b884c18cb0513e5
atmosphere8 <azurrodneyyy@gmail.com>
2023-12-25 21:30:20 +0200

    map update

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -48,44 +48,6 @@ function Events() {
             </div>
           </div>
         ))}
-        {response.map((event) => (
-          <div className="event" key={event.id}>
-            <img className="event__image" src={event.imagePath} alt="" />
-
-            <div className="event__text">
-              <div className="event__location">
-                <img className="location__point" src={Point} alt="" />
-                <p className="location__text">{event.locationText}</p>
-              </div>
-              <h2 className="text__main">{event.title}</h2>
-              <p className="text__description">{event.description}</p>
-              <div className="event__button">
-                <a className="button__item" href="">
-                  <p className="button__text">MORE ABOUT EVENT</p>
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
-        {response.map((event) => (
-          <div className="event" key={event.id}>
-            <img className="event__image" src={event.imagePath} alt="" />
-
-            <div className="event__text">
-              <div className="event__location">
-                <img className="location__point" src={Point} alt="" />
-                <p className="location__text">{event.locationText}</p>
-              </div>
-              <h2 className="text__main">{event.title}</h2>
-              <p className="text__description">{event.description}</p>
-              <div className="event__button">
-                <a className="button__item" href="">
-                  <p className="button__text">MORE ABOUT EVENT</p>
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
       </div>
     </section>
   );
